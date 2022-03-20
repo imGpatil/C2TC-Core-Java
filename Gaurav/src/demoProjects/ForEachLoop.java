2fc7cf87133e5954be8574f61fbd0529cc197e3b
gaurav <gaurav@DESKTOP-UO5KS4V>
2022-03-20 21:46:28 +0530

    loop

---
+++ Gaurav/src/demoProjects/ForEachLoop.java
@@ -0,0 +1,14 @@
+package demoProjects;
+
+public class ForEachLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] car = {"BMW", "Audi" , "Mercedes"};
+		String[] nation = {"India", "China", "USA"};
+		for (String element: car)
+		{
+			System.out.println(element);
+		}
+		}
+}
